3d004ab7b8dbe69098cfbde3fc43109ea2c12443
Dillip-Jena <dillip.jena2004@gmail.com>
2026-04-24 15:47:12 +0530

    Fix main method name and adjust size variable
    Changed the main method name and updated size variable.

--- 1_Patterns/Question5.java
+++ 1_Patterns/Question5.java
@@ -10,8 +10,8 @@
 //    1     4
 
 public class Question5 {
-    public static void main(String[] args){
-        int size = 7;
+    public static void mai(String[] args){
+        int size = 5;
         for(int i=1; i<=size; i++){
             for(int j=1; j<i; j++){
                 System.out.print(" ");
