60db7f747b80d57cf391a88cbf8e3d416367cbb7
Dillip-Jena <dillip.jena2004@gmail.com>
2026-04-24 15:49:11 +0530

    Fix typo in main method name

--- 1_Patterns/Question5.java
+++ 1_Patterns/Question5.java
@@ -10,7 +10,7 @@
 //    1     4
 
 public class Question5 {
-    public static void mai(String[] args){
+    public static void main(String[] args){
         int size = 5;
         for(int i=1; i<=size; i++){
             for(int j=1; j<i; j++){
